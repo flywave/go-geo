1d829acc3378a0c0392325c2be664954c1786b52
hj <hawking2215@gmail.com>
2021-10-15 15:41:26 +0800

    update

--- srs.go
+++ srs.go
@@ -18,6 +18,7 @@ import (
 const (
 	SRS_3857 = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over"
 	SRS_4326 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +over"
+	SRS_ECEF = "+proj=geocent +datum=WGS84 +units=m +no_defs"
 )
 
 var (
